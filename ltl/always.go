1f09c6be042ba7e0e2f9373dc22fe40809a85557
tympanix <tympanix@gmail.com>
2019-09-09 14:13:11 +0200

    add: always.go and eventually.go

---
+++ ltl/always.go
@@ -0,0 +1,29 @@
+package ltl
+
+import "fmt"
+
+// Always is the structure for LTL Always
+type Always struct {
+	Child Node
+}
+
+// ChildNode returns the child node of tha Always op
+func (a Always) ChildNode() Node {
+	return a.Child
+}
+
+func (a Always) String() string {
+	if _, ok := a.ChildNode().(BinaryNode); ok {
+		return fmt.Sprintf("[](%v)", a.ChildNode())
+	}
+	return fmt.Sprintf("[]%v", a.ChildNode())
+}
+
+// SameAs returns true if both nodes are ltl Always and has
+// identical sub-tree
+func (a Always) SameAs(node Node) bool {
+	if n, ok := node.(Always); ok {
+		return a.ChildNode().SameAs(n.ChildNode())
+	}
+	return false
+}
